de34125a3c5eb2a112b62afad0f47d0c4ced3ae6
lucaslija <81848948+lucaslija@users.noreply.github.com>
2024-05-20 17:32:32 -0700

    added contact content, began to style

--- src/contact.js
+++ src/contact.js
@@ -14,15 +14,55 @@ function setContact(contentElement) {
   // Splash
   contentArray.push(splash);
 
-  // Photogrid
-
-  // Reviews
+  // Contact
+  contentArray.push(getContact());
 
   // Footer 
   contentArray.push(footer);
 
   // Append to DOM
   contentElement.append(...contentArray);
+}
+
+function getContact() {
+  const contact = document.createElement("div");
+  contact.id = "contact";
+
+  const contactTitle = document.createElement("h1");
+  contactTitle.id = "contact-title";
+  contactTitle.textContent = "CONTACT US";
+
+  const contactMessage = document.createElement("h5");
+  contactMessage.id = "contact-message";
+  contactMessage.textContent = "to make a reservation, please contact us by phone up to a week in advance"
+
+  const contactInfo = document.createElement("div");
+  contactInfo.id = "contact-info";
+
+  const phoneLabel = document.createElement("p");
+  phoneLabel.classList.add = "contact-label";
+  phoneLabel.textContent = "by phone:"
+  const phone = document.createElement("p");
+  phone.textContent = "(503) 753 2047";
+  const emailLabel = document.createElement("p");
+  emailLabel.classList.add = "contact-label";
+  emailLabel.textContent = "by email:";
+  const email = document.createElement("p");
+  email.textContent = "tinroofcafe@totally-real-email.com";
+  const addressLabel = document.createElement("p");
+  addressLabel.classList.add = "contact-label";
+  addressLabel.textContent = "by snail mail:"
+  const address = document.createElement("p");
+  address.textContent = "1111 NS 11st Ave, Portland, OR 97XXX";
+  const contactMethods = [phoneLabel, phone, emailLabel, email, addressLabel, address];
+
+  contactInfo.append(...contactMethods);
+
+  const contactSections = [contactTitle, contactMessage, contactInfo];
+
+  contact.append(...contactSections);
+  
+  return contact;
 }
 
 export default setContact;
